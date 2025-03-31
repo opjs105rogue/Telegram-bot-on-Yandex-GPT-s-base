5fb9bdd0c28fd3268ba8f78adfcb3f62de92805a
Toxa-K <originkewai666.com@mail.ru>
2025-03-31 16:13:56 +0300

    GPTbot step №5

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import dotenv from 'dotenv';
 import {getTextCompletion} from './utils/yandexFetches.js';
 import { repository } from './utils/getIamTokenUtil.js';
 import { Telegraf } from 'telegraf';
-import { message } from 'telegraf/filters';
+import { message } from 'telegraf/filters'; //-
+import {getReplyId} from './utils/telegramModules.js'
 
 dotenv.config();
 
@@ -24,12 +25,61 @@ await bot.telegram.setMyCommands([
 ])
 
 
-bot.on(message, (ctx) => ctx.reply('Hello World!'))
+bot.on('message', async (ctx) => {
+    const replyId = getReplyId(ctx);
+
+    switch(ctx.text) {
+        case '/role':
+            contextStore[replyId] = contextStore[replyId] || DEFAULT_USER_STATE;
+            contextStore[replyId].isWaitungFor = "role";
+            return ctx.reply('Выберите роль помощника');
+        default:
+            const isWaitungForRole = contextStore[replyId]?.isWaitungFor === 'role';
+            if(isWaitungForRole) {
+                contextStore[replyId].isWaitungFor = null;
+                contextStore[replyId].role = ctx.text;
+                return ctx.reply(`Для помощника выбрана роль: ${ctx.text}`);
+            }
+
+            if(!contextStore[replyId]) {
+                return ctx.reply(`Нет контекста для пользователя с ID: ${replyId}`)
+            }
+
+            ctx.reply(await getTextCompletion({
+                iamToken: repository.value,
+                folderId: process.env.FOLDER_ID,
+                model: 'yandexgpt/latest',
+                role: contextStore[replyId].role,
+                message: [
+                    {
+                        role: 'user',
+                        text: ctx.text
+                    }
+                ]
+            }))
+            
+            ctx.reply(alternatives[0].message.text, {parse_mode: 'MarkdownV2'})
+
+            return ctx.reply(``)
+    }})
 bot.launch()
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+/** 
+*@type{Record<number, { isWaitungFor?: 'role'; role:string; messages: Array<{role: 'user' | 'assistant'; text: string }>}>} 
+*/
+const contextStore = {
+    
+}
+
+const DEFAULT_USER_STATE = {
+    isWaitungFor: null,
+    role:null,
+    messages: [],
+};
 
 async function main() {
     await repository.init(process.env.OAuthToken)
